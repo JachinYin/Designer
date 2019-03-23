e89c5b65aac90fc1e9bac1b966c316c2a06b8a6a
JachinYin <1106025235@qq.com>
2019-03-23 23:39:14 +0800

    通用类完善

--- src/main/java/com/jachin/des/entity/SearchArg.java
+++ src/main/java/com/jachin/des/entity/SearchArg.java
@@ -6,6 +6,7 @@ package com.jachin.des.entity;
  * @since 2019/3/16 15:27
  */
 public class SearchArg {
+    private int id;
     private int aid;
     private int tempId;
     private String title;
@@ -13,9 +14,21 @@ public class SearchArg {
     private String begTime;
     private String endTime;
     private int status;
-    private String dName;
+    private String nickName;
+
+    private boolean distinct;     // 是否去重
+    private String columns;       // 排序列
+    private boolean comp;         // 是否规则
 
     public SearchArg() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getAid() {
@@ -74,11 +87,41 @@ public class SearchArg {
         this.status = status;
     }
 
-    public String getDName() {
-        return dName;
+    public String getNickName() {
+        return nickName;
     }
 
-    public void setDName(String dName) {
-        this.dName = dName;
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getColumns() {
+        return columns;
+    }
+
+    public void setColumns(String columns) {
+        this.columns = columns;
+    }
+
+    public boolean isComp() {
+        return comp;
+    }
+
+    public void setComp(boolean comp) {
+        this.comp = comp;
+    }
+
+    public boolean isDistinct() {
+        return distinct;
+    }
+
+    public void setDistinct(boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    // 设置排序规则
+    public void setCompColumns(String column, boolean desc){
+        this.columns = column;
+        this.comp = desc;
     }
 }
