72f050906c4db6e08585328c40cc2f2132acf934
JachinYin <1106025235@qq.com>
2019-03-25 00:07:46 +0800

    映射图片请求文件到本地文件系统

--- src/main/java/com/jachin/des/config/WebAppConfig.java
+++ src/main/java/com/jachin/des/config/WebAppConfig.java
@@ -1,19 +1,59 @@
 package com.jachin.des.config;
 
 import com.jachin.des.interceptor.LoginInterceptor;
+import com.jachin.des.util.CommTool;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * @author Jachin
  * @since 2019/3/9 17:12
  */
 @Configuration
-public class WebAppConfig implements WebMvcConfigurer {
+//public class WebAppConfig implements WebMvcConfigurer {
+public class WebAppConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         // 注册拦截器
         registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
     }
+
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大KB,MB
+//        factory.setMaxFileSize("10MB");
+        //设置总上传数据总大小
+//        factory.setMaxRequestSize("10MB");
+        return factory.createMultipartConfig();
+    }
+
+    /**
+     * 这里是映射文件路径的方法
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        String os = System.getProperty("os.name");
+
+        if (os.toLowerCase().startsWith("win")) {  //如果是Windows系统
+            registry.addResourceHandler("/Img/**")
+                    // /apple/**表示在磁盘apple目录下的所有资源会被解析为以下的路径
+                    .addResourceLocations("file:" + CommTool.imgUrl + "/") //媒体资源
+                    .addResourceLocations("classpath:/META-INF/resources/");  //swagger2页面
+        } else {  //linux 和mac
+            registry.addResourceHandler("/smallapple/**")
+                    .addResourceLocations("file:/resources/smallapple/")   //媒体资源
+                    .addResourceLocations("classpath:/META-INF/resources/");  //swagger2页面;
+        }
+    }
+
 }
