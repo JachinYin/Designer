905b958bcc102fbd751632297d8813d48b64efa0
JachinYin <1106025235@qq.com>
2019-04-11 00:04:45 +0800

    基础支撑

--- src/main/java/com/jachin/des/util/ResParam.java
+++ src/main/java/com/jachin/des/util/ResParam.java
@@ -1,11 +1,14 @@
 package com.jachin.des.util;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 
 /**
  * @author Jachin
  * @since 2019/3/13 14:11
  */
+@Component
 public class ResParam extends HashMap<String, Object> {
 
     public ResParam(){}
