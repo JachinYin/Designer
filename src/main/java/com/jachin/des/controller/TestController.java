4bcf5711149cd83f23423e918677424022f39b34
Jachin <JachinQ@qq.com>
2019-04-10 18:02:52 +0800

    test

--- src/main/java/com/jachin/des/controller/TestController.java
+++ src/main/java/com/jachin/des/controller/TestController.java
@@ -1,12 +1,9 @@
 package com.jachin.des.controller;
 
 import com.jachin.des.entity.SearchArg;
-import com.jachin.des.entity.User;
 import com.jachin.des.mapper.TemplateAuditMapper;
-import com.jachin.des.mapper.provider.SqlUtils;
 import com.jachin.des.service.TemplateAuditService;
 import com.jachin.des.service.UserService;
-import com.jachin.des.util.ResParam;
 import com.jachin.des.util.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,12 +27,6 @@ public class TestController {
 
     @GetMapping("getTest")
     public Response getTest(SearchArg searchArg){
-        User user = userService.getUser(searchArg);
-        Response response = new Response(true);
-        ResParam resParam = new ResParam();
-        resParam.put("user", user);
-        resParam.put("sql", SqlUtils.userSql.getUser(searchArg));
-        response.setData(resParam);
-        return response;
+        return new Response(true);
     }
 }
